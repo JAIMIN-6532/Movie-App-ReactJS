5c515db7607a881aed84880c09e740bb5cb045d5
Jaimin Rathod <123061180+JAIMIN-6532@users.noreply.github.com>
2024-09-09 19:53:37 +0530

    toggle Btn for favourite and add to card added seventh commit

--- src/Movie-card.js
+++ src/Movie-card.js
@@ -12,7 +12,9 @@ class MovieCard extends React.Component{
                     'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
                 rating: '8.0',
                 price: 199,
-                star: 0
+                star: 0,
+                fav: false,
+                isInCart: false
         } 
 
         //Binding the event handler in the constructor if event handler is not an arrow function
@@ -52,9 +54,23 @@ class MovieCard extends React.Component{
     })
   }
 
+  //Toggle Favourite button
+  toggleFav= () => {
+    this.setState({
+        fav: !this.state.fav
+    })
+  }
+  
+  //Toggle add to cart button
+  toggleCart= () => {
+    this.setState({
+        isInCart: !this.state.isInCart
+    })
+ }
+
     render(){
         //Destructing the state object in render function
-        const {title, plot, poster, price, rating,star} =  this.state;
+        const {title, plot, poster, price, rating,star,fav,isInCart} =  this.state;
           
         return(
             <div className="main">
@@ -99,9 +115,20 @@ class MovieCard extends React.Component{
                                 <span className="starCount">{star}</span>
                             </div>
 
-                            {/**Favourite and add to cart buttons */}
-                            <button className="favourite-btn">Favourite</button>
-                            <button className="cart-btn">Add to Cart</button>
+                            {/**conditional rendering on Favourite button */}
+                            <button className={fav?"unfavourite-btn":"favourite-btn"}  onClick={this.toggleFav}>
+                                {fav ? "Un-favourite":"Favourite"}
+                            </button>
+                            {/**
+                             * {fav? <button className="unfavourite-btn" onClick={this.toggleFav}> Un-favourite </button> :
+                             *       <button className="favourite-btn" onClick={this.toggleFav}> favourite </button>}
+                             */}
+
+                             {/**Conditional Rendering on Add to Cart Button */}
+                            <button className={isInCart?"unfavourite-btn":"cart-btn"}  onClick={this.toggleCart}>
+                                {isInCart ? "Remove from Cart":"Add to Cart"}
+                            </button>
+
                             
                         </div>
                     </div>
