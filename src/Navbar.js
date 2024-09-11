e88201ede50ec028f5029102129ff38a727fa79c
Jaimin Rathod <123061180+JAIMIN-6532@users.noreply.github.com>
2024-09-11 19:28:50 +0530

    code Structure Updated twelth commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,67 +1,24 @@
 import React from "react";
-// import './index.css';
-import styled from "styled-components"
-
-const Nav =  styled.div`
-    height: 70px;
-	background: linear-gradient(170deg, #1bc059, #0d47a1);
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	position: relative;
-  
-`;
-
-const Title = styled.div`
-    font-size: 30px;
-    color: #fff;
-    font-weight: 600;
-    font-family: 'Times New Roman', Times, serif;
-    text-transform: uppercase;
-    margin-left: 20px ;
-`;
-
-
-const CartImg = styled.img`
-    height: 48px;
-    margin-right: 20px;
-`;
-
-const CartIconContainer = styled.div`
-    position: relative;
-    cursor: pointer;
-`;
-
-const CartCount = styled.div`
-    background: yellow;
-    border-radius: 50%;
-    padding: 4px 8px;
-    position: absolute;
-    right: 10px;
-    top: -5px;
-    font-size: 12px;
-`;
-
-
+import styles from './nav.module.css'
 
 class Navbar extends React.Component {
 
-  
     render() {   
        
       return (
       <>
-      <Nav>
+      <div className={styles.nav}>
         
-      <Title>MOVIE APP</Title>
+      <div className={styles.title}>MOVIE APP</div>
         
-        <CartIconContainer>
-            <CartImg alt="Cart-Icon" 
+        <div className={styles.cartIconContainer}>
+            <img className={styles.cartIcon} 
+                 alt="Cart-Icon" 
                  src = "https://cdn-icons-png.flaticon.com/128/891/891462.png"    
             />
-            <CartCount >5</CartCount>
-        </CartIconContainer>
-      </Nav>
+            <span className ={styles.cartCount}>0</span>
+        </div>
+      </div>
 
       </>
       )
